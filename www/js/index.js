d8851c915d84080a185b2ef3ebc49ceedb952c97
Alex Covizzi <alexcovizzi@gmail.com>
2018-06-03 20:11:32 +0200

    moved assets/ to www/

---
+++ www/js/index.js
@@ -0,0 +1,20 @@
+(function() {
+    /*
+    const vscode = acquireVsCodeApi();
+
+    window.addEventListener("load", eventWindowLoad);
+    window.addEventListener('message', eventWindowMessage);
+
+    function eventWindowLoad() {
+        //
+    }
+
+    function eventWindowMessage(event) {
+        const message = event.data;
+
+        switch (message.command) {
+            
+        }
+    }
+    */
+})();
