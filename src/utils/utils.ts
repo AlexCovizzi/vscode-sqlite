5612a35214aea5bc0d69cf0725a6ba6ced0f7a47
Alex Covizzi <alexcovizzi@gmail.com>
2018-06-03 20:22:32 +0200

    added mothod to get a random string

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -31,3 +31,7 @@ export function getSqlitePath(extensionPath: string) {
         return '';
     }
 }
+
+export function randomString(length: number) {
+    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+}
