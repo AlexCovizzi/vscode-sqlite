d5bd082f7be4bc5bbc1b3c0d1bd1c59f7cdadcef
Alex Covizzi <alexcovizzi@gmail.com>
2018-06-03 20:24:24 +0200

    added prompts utility for every command that requires the command palette

---
+++ src/prompts/prompts.ts
@@ -0,0 +1,71 @@
+import { QuickPickItem, Uri, workspace, window } from 'vscode';
+import { basename } from 'path';
+import { DatabaseStore } from '../database/databaseStore';
+
+namespace QuickPick {
+    export class DatabaseItem implements QuickPickItem {
+        uri: Uri;
+        label: string;
+        description: string;
+        detail?: string | undefined;
+        picked?: boolean | undefined;
+        
+        constructor(uri: Uri) {
+            this.uri = uri;
+            this.label = basename(uri.fsPath);
+            this.description = uri.fsPath;
+        }
+    }
+}
+
+
+export function showOpenDatabaseQuickPick(onPicked?: (path: string) => void) {
+    const promise = new Promise<QuickPick.DatabaseItem[]>((resolve, reject) => {
+        workspace.findFiles('**/*.db', '**/*.sqlite').then((filesUri) => {
+            if (filesUri.length === 0) {
+                reject("No database found");
+            }
+            resolve(filesUri.map(uri => new QuickPick.DatabaseItem(uri)));
+        });
+    });
+    window.showQuickPick(promise).then(
+        item => {
+            if (item && onPicked) {
+                onPicked(item.uri.fsPath);
+            }
+        }
+    );
+}
+
+export function showCloseDatabaseQuickPick(databaseStore: DatabaseStore, onPicked?: (path: string) => void) {
+    const promise = new Promise<QuickPick.DatabaseItem[]>((resolve, reject) => {
+        if (databaseStore.getAll().length === 0) {
+            reject("No database open");
+        }
+        resolve(databaseStore.getAll().map(db => new QuickPick.DatabaseItem(Uri.parse(db.dbPath))));
+    });
+    window.showQuickPick(promise).then(
+        item => {
+            if (item && onPicked) {
+                onPicked(item.uri.fsPath);
+            }
+        }
+    );
+}
+
+
+export function showBindDatabaseQuickPick(databaseStore: DatabaseStore, onPicked?: (path: string) => void) {
+    const promise = new Promise<QuickPick.DatabaseItem[]>((resolve, reject) => {
+        if (databaseStore.getAll().length === 0) {
+            reject("No database open");
+        }
+        resolve(databaseStore.getAll().map(db => new QuickPick.DatabaseItem(Uri.parse(db.dbPath))));
+    });
+    window.showQuickPick(promise).then(
+        item => {
+            if (item && onPicked) {
+                onPicked(item.uri.fsPath);
+            }
+        }
+    );
+}
