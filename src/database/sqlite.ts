8910d816d2f2a3ff09cf91b3f1c838485b0c40cf
Alex Covizzi <alexcovizzi@gmail.com>
2018-06-03 20:16:56 +0200

    the query is now divided in statements, every statement is executed by dblite module

--- src/database/sqlite.ts
+++ src/database/sqlite.ts
@@ -1,12 +1,12 @@
 'use strict';
 
 import { existsSync } from 'fs';
-import { basename } from 'path';
 import { EventEmitter } from 'events';
 import { Disposable } from 'vscode';
-import * as DBLite from 'dblite';
 import { OutputLogger } from '../logging/logger';
-import { ResultSet, Row } from './resultSet';
+import { ResultSet, Row, Result } from './resultSet';
+import { SQLParser } from './sqlparser';
+var DBLite = require('./dblite');
 
 
 export class Database extends EventEmitter implements Disposable {
@@ -25,12 +25,9 @@ export class Database extends EventEmitter implements Disposable {
         }
 
         if (existsSync(dbPath)) {
-            /*  In node_modules/dblite.node.js (438:3)
-                set self.ignoreErrors = true
-                (this way if there is an error dblite is not blocked)
-            */
             DBLite.bin = sqlitePath;
             this.dblite = new DBLite(dbPath, '-header');
+            this.dblite.query('.nullvalue NULL');
         } else {
             callback(new Error(`Failed to open '${dbPath}': file does not exist.`));
         }
@@ -49,79 +46,52 @@ export class Database extends EventEmitter implements Disposable {
         });
     }
 
-    exec(query: string, callback?: (result: ResultSet, err: Error | null) => void) {
-        let queries = this.parse(query);
+    exec(query: string, callback?: (resultSet: ResultSet) => void) {
+        let stmts = SQLParser.parse(query);
         
         let resultSet = new ResultSet();
 
-        this.queue(queries, resultSet, callback);
+        stmts.forEach( (stmt, i) => {
+            this.execStmt(stmt, result => {
+                if (result) {
+                    resultSet.push(result);
+                }
+                if (i === stmts.length-1) {
+                    if (callback) {
+                        callback(resultSet);
+                    }
+                }
+            });
+        });
     }
 
-    private queue(queries: string[], resultSet: ResultSet, callback?: (result: ResultSet, err: Error | null) => void) {
-        if (queries.length === 0) {
-            if (callback) {
-                callback(resultSet, null);
-            }
-            return;
-        }
+    private execStmt(stmt: string, callback: (result: Result | undefined) => void) {
+        OutputLogger.log(`${stmt}`, `QUERY][${this.dbPath}`);
 
-        let curQuery = queries.shift();
-
-        OutputLogger.log(`${curQuery}`, `QUERY][${this.dbPath}`);
-
-        this.dblite.query(curQuery, (err: Error | null, rows: Object[]) => {
+        this.dblite.query(stmt, (err: Error | null, rows: Object[]) => {
             if (err) {
-                console.log(err.message);
-                resultSet.push({header: ['error'], rows: [{error: err.message}] });
+                OutputLogger.error(`${err.message}`);
+                callback(undefined);
             } else {
+                let result: Result | undefined = undefined;
                 if (rows) {
                     let header = Object.keys(rows[0]);
                     let resultRows: Row[] = [];
                     rows.forEach(row => {
-                        let resultRow: Row = {};
+                        let resultRow: Row = [];
                         header.forEach(col => {
-                            resultRow[col] = (<any>row)[col];
+                            resultRow.push((<any>row)[col]);
                         });
                         resultRows.push(resultRow);
                     });
-                    resultSet.push({header: header, rows: resultRows});
+                    result = {header: header, rows: resultRows};
                 }
+                callback(result);
             }
-            this.queue(queries, resultSet, callback);
         });
-    }
-
-    private parse(query: string): string[] {
-        query = query.trim();
-        let queries = query.split(";");
-        queries = queries.map(q => q.trim());
-        queries = queries.filter(q => q.length > 0);
-        queries = queries.map(q => q.concat(";"));
-        return queries;
     }
 
     dispose() {
         this.dblite.close();
-    }
-}
-
-export class SQLScript {
-    queries: string[];
-
-    constructor(script: string) {
-        script = script.trim();
-        script = this.sanitize(script);
-
-        let queries: string[] = [];
-        script.split(';').forEach(element => {
-            queries.push(element.trim());
-        });
-        queries = queries.filter(x => x !== '');
-        this.queries = queries;
-    }
-
-    private sanitize(s: string): string {
-        s = s.trim();
-        return s;
     }
 }
