462e391998c2bfa69426027c49b590a2a7121431
AlexCovizzi <alexcovizzi@gmail.com>
2018-12-23 13:29:01 +0100

    moved sqlite command validation from module configuration to module sqlite

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,5 +1,4 @@
 import { workspace } from "vscode";
-import { validateOrFallback } from "../utils/cmdSqlite3Utils";
 import { Level } from "../logging/logger";
 
 const properties = require('../../package.json').contributes.configuration.properties;
@@ -10,19 +9,17 @@ export interface Configuration {
     recordsPerPage: number;
 }
 
-export function getConfiguration(extensionPath: string) {
+export function getConfiguration() {
     return {
-        sqlite3: _sqlite3(extensionPath),
+        sqlite3: _sqlite3(),
         logLevel: _logLevel(),
         recordsPerPage: _recordsPerPage()
     } as Configuration;
 }
 
-function _sqlite3(extensionPath: string): string {
+function _sqlite3(): string {
     let sqlite3Conf = workspace.getConfiguration().get('sqlite.sqlite3');
     let sqlite3: string | undefined = sqlite3Conf? sqlite3Conf.toString() : '';
-    sqlite3 = validateOrFallback(sqlite3, extensionPath);
-    sqlite3 = sqlite3? sqlite3 : "";
 
     return sqlite3;
 }
