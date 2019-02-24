713a485c26724e7ce62799ee6ed42eb0893fac1d
AlexCovizzi <alexcovizzi@gmail.com>
2019-02-24 17:52:20 +0100

    fixed jest matchers typing

--- tests/typings/matchers.d.ts
+++ tests/typings/matchers.d.ts
@@ -1,9 +1,10 @@
 import { TreeDataProvider } from "vscode";
 import { DatabaseFixture } from "../helpers/fixtureHelper";
 
-
-declare namespace jest {
-    interface Matchers<R> {
-        toInclude(databaseFixture: DatabaseFixture): TreeDataProvider<any>;
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toInclude(databaseFixture: DatabaseFixture): TreeDataProvider<any>;
+        }
     }
 }
