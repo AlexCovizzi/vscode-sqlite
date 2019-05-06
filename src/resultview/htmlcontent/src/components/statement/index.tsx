cb508f5a79e84b91991771d4892969a0489e7ce2
AlexCovizzi <alexcovizzi@gmail.com>
2019-05-06 15:51:59 +0200

    the statement in the result view is now collapsed in a single line by default, and can be expanded clicking on it (#67)

--- src/resultview/htmlcontent/src/components/statement/index.tsx
+++ src/resultview/htmlcontent/src/components/statement/index.tsx
@@ -7,16 +7,27 @@ interface Props {
 }
 
 interface State {
-
+    isCollapsed: boolean;
 }
 
 export class Statement extends Component<Props, State> {
 
+    constructor(props: Props, context: any) {
+        super(props, context);
+
+        this.state = ({isCollapsed: true});
+    }
+
+    onclick() {
+        const currState = this.state.isCollapsed;
+        this.setState({isCollapsed: !currState});
+    }
+
     render(props: Props, state: State) {
         return (
-            <Fetch resource={`resultSet/${props.idx}/stmt`}>
+            <Fetch resource={`resultSet/${props.idx}/stmt`} forceUpdate={true}>
                 {(response: Response) => (
-                    <div>
+                    <div class={state.isCollapsed ? style.statementCollapsed : null} onClick={() => this.onclick()}>
                         {response.loading && <code>Loading...</code>}
                         {response.error &&  <code>Error: {response.error}</code>}
                         {response.data && <code>{response.data}</code>} 
